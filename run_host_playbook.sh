893025cbdf472c86a4b479a664e786590db0272b
Dominik Holler <dholler@redhat.com>
2020-02-06 08:40:36 +0100

    Improve robustness and execution time

--- run_host_playbook.sh
+++ run_host_playbook.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
 source auth.sh
+
 for i in $(seq 1 95) ; do
-  ansible-playbook -e $(printf host_name=ovirt-host-%02d $i) create_host.yml &
-  sleep 50
+  ansible-playbook -e $(printf host_name=ovirt-host-%02d $i) create_host.yml || \
+      ansible-playbook -e $(printf host_name=ovirt-host-%02d $i) create_host.yml &
+  sleep $((i*4 < 60 ? i*4 : 60))
 done
 
 # kill -f create_host.yml
