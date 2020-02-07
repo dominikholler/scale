f77716eb66a8c660b07de9d00dd71226aa6337f5
Dominik Holler <dholler@redhat.com>
2020-02-07 22:29:47 +0100

    improved update handling
    not tested

--- prepare_engine.sh
+++ prepare_engine.sh
@@ -5,6 +5,7 @@ source auth.sh
 engine=$1
 SSH="ssh root@$engine -o StrictHostKeyChecking=no"
 $SSH yum install -y $OVIRT_REPO
+$SSH yum update -y
 $SSH yum install -y ovirt-engine
 $SSH -T "cat > /root/answerfile.txt" <<EOF
 [environment:default]
@@ -12,5 +13,6 @@ OVESETUP_CONFIG/fqdn=str:$OVIRT_HOSTNAME
 OVESETUP_CONFIG/adminPassword=str:$OVIRT_PASSWORD
 EOF
 $SSH engine-setup --accept-defaults --offline --config-append=/root/answerfile.txt
+$SSH engine-config -s HostPackagesUpdateTimeInHours=0
 sleep 10
 ansible-playbook add_networks.yml
